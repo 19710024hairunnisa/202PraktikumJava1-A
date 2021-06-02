c9a362d800cdeb8e63c5d4c5635f160648f3a1bf
Your Name <19710024hairunnisa@gmail.com>
2021-06-02 09:40:09 +0800

    Deklarasi Variable dan Tipe Data Output

--- DataTypes.java
+++ DataTypes.java
@@ -1,11 +1,19 @@
 public class DataTypes{
   public static void  main(String[]args){
       String namaDepan = "Hairun";
-      String namaTengah  = "Nisa";
+      String namaBelakang  = "Nisa";
       int usia = 18;
       int targetTahunKuliah = 4;
       double ipl = 3.8998;
       char nilaiAbjad = 'A';
       boolean cantik = true;
+
+      System.out.println("Nama Lengkap : " + " " 
+	+ namaDepan +" " +namaBelakang);
+
+      System.out.println("Usia : " + usia + " Tahun");
+      System.out.println("Usia ketika lulus : " + (usia + targetTahunKuliah) + " Tahun");
+
+
   }
 }
