e59d82521bffcc94825b2b2c677b567ba011c7ca
Your Name <19710024hairunnisa@gmail.com>
2021-06-02 09:20:51 +0800

    Deklarasi Variable dan Tipe Data

---
+++ DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes{
+  public static void  main(String[]args){
+      String namaDepan = "Hairun";
+      String namaTengah  = "Nisa";
+      int usia = 18;
+      int targetTahunKuliah = 4;
+      double ipl = 3.8998;
+      char nilaiAbjad = 'A';
+      boolean cantik = true;
+  }
+}
